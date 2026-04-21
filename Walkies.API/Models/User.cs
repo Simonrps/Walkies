1d819a5a36320a78c2d5e2b344ec216d07ef775c
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-04-21 15:21:45 +0100

    Updated folder structure and removed template files

---
+++ Walkies.API/Models/User.cs
@@ -0,0 +1,8 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Walkies.API.Models
+{
+    public class User
+    {
+    }
+}
--- Walkies.MAUI/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-global using Fonts;
-global using Walkies.MAUI.Data;
-global using Walkies.MAUI.PageModels;
-global using Walkies.MAUI.Pages;
-global using Walkies.MAUI.Services;
-global using Walkies.MAUI.Utilities;
