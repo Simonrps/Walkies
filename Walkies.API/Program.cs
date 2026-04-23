85b48659ea563c694b3c450e21f7e1a04ccaa8a2
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-04-23 16:32:45 +0100

    Fix dotnet format violations - trailing newlines and import ordering

--- Walkies.API/Program.cs
+++ Walkies.API/Program.cs
@@ -1,7 +1,7 @@
-using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Walkies.API.Data;
 
 /// <summary>
