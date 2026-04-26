78035bed887b75253a4fa547c8ce220975b9d61b
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-04-26 23:31:53 +0100

    feat(users-dogs): Add DTOs for UsersController and DogsController

---
+++ Walkies.API/DTOs/UpdateUserDto.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Walkies.API.DTOs
+{
+    /// <summary>
+    /// Data Transfer object for updating user information.
+    /// Comtains the fields the user can modify on their profile
+    /// Related to US03 - Profile Management
+    /// </summary>
+    public class UpdateUserDto
+    {
+        /// <summary>
+        /// getss or sets the first name of the user.
+        /// </summary>
+        [Required, MaxLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// getss or sets the users last name.
+        /// </summary>
+        [Required, MaxLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// getss or sets the users phone number.
+        /// </summary>
+        [MaxLength(20)]
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// getss or sets the users address.
+        /// </summary>
+        [MaxLength(300)]
+        public string? Address { get; set; }
+
+        /// <summary>
+        /// gets or set the latitude of the user
+        /// used to calculate the distance between users
+        /// </summary>
+        public double? Latitude { get; set; }
+
+        /// <summary>
+        /// gets or set the Longitude of the user
+        /// used to calculate the distance between users
+        /// </summary>
+        public double? Longitude { get; set; }
+    }
+}
---
+++ Walkies.API/DTOs/UserProfileDto.cs
@@ -0,0 +1,62 @@
+﻿namespace Walkies.API.DTOs
+{
+    /// <summary>
+    /// Data transfer object for user profile information.
+    /// Contains the public-facing profiledata for a registered user.
+    /// Related to US03 - Profile Management
+    /// </summary>
+    public class UserProfileDto
+    {
+        /// <summary>
+        /// gets or sets the unique identifier of the user profile.
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// gets or sets the firstname of the user
+        /// </summary>
+        public string FirstName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// gets or sets the lsstname of the user
+        /// </summary>
+        public string LastName { get; set; } = string.Empty;
+
+        /// <summary>
+        /// gets or sets the users email address
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// gets or sets the users role
+        /// </summary>
+        public string Role { get; set; } = string.Empty;
+
+        /// <summary>
+        /// gets or sets the users phone number
+        /// </summary>
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// gets or sets the users address
+        /// </summary>
+        public string? Address { get; set; }
+
+        /// <summary>
+        /// gets or sets the latitude of the user
+        /// Calculates the proximity between owners and walkers
+        /// </summary>
+        public double? Latitude { get; set; }
+
+        /// <summary>
+        /// gets or sets the Longitude of the user
+        /// Calculates the proximity between owners and walkers
+        /// </summary>
+        public double? Longitude { get; set; }
+
+        /// <summary>
+        /// gets or set the date and time the account was created
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+    }
+}
