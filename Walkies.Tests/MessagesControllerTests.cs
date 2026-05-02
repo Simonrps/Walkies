29a4b50efe0c11ffaecee4c79906d7250341e834
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-05-02 22:28:33 +0100

    feat(messages): Implement SendMessage endpoint - green

--- Walkies.Tests/MessagesControllerTests.cs
+++ Walkies.Tests/MessagesControllerTests.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Walkies.API.Controllers;
 using Walkies.API.Data;
 using Walkies.API.DTOs;
