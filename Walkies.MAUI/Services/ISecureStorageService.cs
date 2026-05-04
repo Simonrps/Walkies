faa2ff3f53028483504f4c2e1d29605dc8d26cec
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-05-05 00:13:30 +0100

    feat(maui): Add AuthService with SecureStorage abstraction

---
+++ Walkies.MAUI/Services/ISecureStorageService.cs
@@ -0,0 +1,13 @@
+﻿namespace Walkies.MAUI.Services
+{
+    /// <summary>
+    /// Absrtaction over SecureStorage to allow instance-based
+    /// access and support for dependency injection
+    /// </summary>
+    public interface ISecureStorageService
+    {
+        Task<string?> GetAsync(string key);
+        Task SetAsync(string key, string value);
+        void Remove(string key);
+    }
+}
