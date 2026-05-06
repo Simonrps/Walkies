9dd76856ddad367fd85e1e7e3a6e9bbeff7b6273
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-05-06 21:00:33 +0100

    feat(maui): Add dog owner page and DogViewModel

--- Walkies.MAUI/Views/OwnerDogsPage.xaml.cs
+++ Walkies.MAUI/Views/OwnerDogsPage.xaml.cs
@@ -1,9 +1,29 @@
+using Walkies.MAUI.ViewModels;
+
 namespace Walkies.MAUI.Views;
 
+/// <summary>
+/// The owner dogs list page. Displays all the dogs on the owners profile
+/// with options to edit or remove. Related to US04 - Add Dog and
+/// US05 - Edit/Remove Dog
+/// </summary>
 public partial class OwnerDogsPage : ContentPage
 {
-    public OwnerDogsPage()
+    private readonly DogViewModel _viewModel;
+
+    public OwnerDogsPage(DogViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
+        _viewModel = viewModel;
+    }
+    
+    /// <summary>
+    /// Loads the owners dogs each time the page appears
+    /// </summary>
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await _viewModel.LoadDogsCommand.ExecuteAsync(null);
     }
 }
