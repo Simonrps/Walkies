eb372b5fbf86d7156396988362074c0a11419b20
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-05-08 11:48:56 +0100

    feat(maui): added OwnerBookingsPages

--- Walkies.MAUI/Views/OwnerBookingsPage.xaml.cs
+++ Walkies.MAUI/Views/OwnerBookingsPage.xaml.cs
@@ -1,9 +1,16 @@
+using Walkies.MAUI.ViewModels;
+
 namespace Walkies.MAUI.Views;
 
-public partial class OwnerBookingsPage : ContentPage
+/// <summary>
+/// Page for owners to view confirmed bookings and upcoming walks.
+/// Related to US13 - View Confirmed Bookings
+/// </summary>
+public partial class OwnerBookingsPage : BasePage
 {
-    public OwnerBookingsPage()
+    public OwnerBookingsPage(BookingViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
     }
 }
