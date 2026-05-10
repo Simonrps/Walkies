a2fd8f6c99284ff67e835585bb90506dde690b35
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-05-10 19:31:07 +0100

    feat(maui): added walker messaging pages, updated mauiprogram, added navigation to dashboards

--- Walkies.MAUI/Views/WalkerMessagesPage.xaml.cs
+++ Walkies.MAUI/Views/WalkerMessagesPage.xaml.cs
@@ -1,9 +1,15 @@
+using Walkies.MAUI.ViewModels;
+
 namespace Walkies.MAUI.Views;
 
-public partial class WalkerMessagesPage : ContentPage
+/// <summary>
+/// Page for Walkers to send and receive messages. Related to US18 - Owner Messaging
+/// </summary>
+public partial class WalkerMessagesPage : BasePage
 {
-    public WalkerMessagesPage()
+    public WalkerMessagesPage(MessagingViewModel viewModel)
     {
         InitializeComponent();
+        BindingContext = viewModel;
     }
 }
