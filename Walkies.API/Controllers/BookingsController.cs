b4a5cfc8c7dd50e863d8d76ae6eef0bd53935112
SimonMulroyWaynua <simonmulroy@waynua.ie>
2026-05-12 22:56:43 +0100

    fix(maui): was getting crash when accepting cancelled bookings.

--- Walkies.API/Controllers/BookingsController.cs
+++ Walkies.API/Controllers/BookingsController.cs
@@ -283,8 +283,10 @@ namespace Walkies.API.Controllers
                 return NotFound(new { message = BookingNotFoundMessage });
             }
 
-            booking.Status = "Cancelled";
             booking.WalkRequest.Status = "Open";
+
+            // remove the booking from db so a new one can be created if the owner wants to rebook with a different walker
+            _context.WalkBookings.Remove(booking);
 
             await _context.SaveChangesAsync();
 
